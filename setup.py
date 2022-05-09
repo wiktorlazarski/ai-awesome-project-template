8532a879f9891c49e52fcd32d2046996c3e0103a
Wiktor Łazarski <wjlazarski@gmail.com>
2022-05-09 20:10:36 +0200

    Update dev requirements installation.

--- setup.py
+++ setup.py
@@ -2,6 +2,15 @@ import runpy
 
 import setuptools
 
+DEV_REQUIREMENTS = [
+    "black",
+    "flake8",
+    "isort",
+    "jupyterlab",
+    "pre-commit",
+    "pytest",
+]
+
 # Parse requirements
 install_requires = [line.strip() for line in open("requirements.txt").readlines()]
 
@@ -28,4 +37,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=install_requires,
+    extras_require={"dev": DEV_REQUIREMENTS},
 )
